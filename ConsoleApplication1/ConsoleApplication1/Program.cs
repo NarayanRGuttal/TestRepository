57f3d7c6ca649dce8ad364118a10963112375c93
NarayanRGuttal <yan_guttal@yahoo.com>
2017-06-08 13:26:10 +0530

    test
    Test

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutomationElement automationElement = null;
+
+            automationElement = AutomationElement.RootElement;
+
+            Condition condition = Condition.TrueCondition;
+
+
+        }
+    }
+}
